f5d8e0f4c3daa392f9cbabd813ec8f9c99d36a24
Binod Bist <thebinodbist@gmail.com>
2025-12-09 19:10:05 +0545

    compoennts created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
-
+import './assets/css/style.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Main from './components/Main'
 function App() {
 
   return (
     <>
-      hello
+      <Header/>
+      <Main/>
+      <Footer/>
     </>
   )
 }
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='text-light'>
+      header
+    </div>
+  )
+}
+
+export default Header
