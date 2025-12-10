da28ad26af0d8c331d05613ce4e6d1d6a1e45186
Binod Bist <thebinodbist@gmail.com>
2025-12-10 19:26:13 +0545

    ...

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.models import User
-from rest_framework import serializer
+from rest_framework import serializers
 
 
-class UserSerializer(serializer.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     password = serializer.CarField(Write_only=True)
     class Meta:
         model  = User
         fields = User
+        fields =['username','email','password']
+
+        def create(self, validated_data):
+            user= User.objects.create_user(**validated_data)
